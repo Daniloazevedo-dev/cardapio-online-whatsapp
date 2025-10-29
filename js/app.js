f175a197d9ab87d8e012d7a8aa44a6e0ebcaea77
Danilo Azevedo <danilo.ann@gmail.com>
2025-10-28 23:56:29 -0300

    feat: Implemenado botões de adicionar iem ao carrinho e quantidade

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 });
 
 var cardapio = {};
+var MEU_CARRINHO = [];
 
 cardapio.eventos = {
     init: () => {
@@ -23,7 +24,8 @@ cardapio.metodos = {
             let temp = cardapio.templates.item
             .replace(/\${img}/g, e.img)
             .replace(/\${nome}/g, e.name)
-            .replace(/\${preco}/g, e.price.toFixed(2).replace('.', ','));
+            .replace(/\${preco}/g, e.price.toFixed(2).replace('.', ','))
+            .replace(/\${id}/g, e.id);
 
             // Botão ver mais foi clicado (12 itens)
             if(vermais && i >= 8 && i < 12) {
@@ -47,13 +49,51 @@ cardapio.metodos = {
         cardapio.metodos.obterItensCardapio(ativo, true);
 
         $("#btnVerMais").addClass('hidden');
+    },
+    dimunuirQuantidade: (id) => {
+        let qntAtual = parseInt($("#qntd-" + id).text());
+        if(qntAtual > 0) {
+            $("#qntd-" + id).text(qntAtual - 1);
+        }
+    },
+    aumentarQuantidade: (id) => {
+        let qntdAtual = parseInt($("#qntd-" + id).text());
+        $("#qntd-" + id).text(qntdAtual + 1);
+    },
+
+    adicionarAoCarrinho: (id) => {
+        let qntdAtual = parseInt($("#qntd-" + id).text());
+        if(qntdAtual > 0) {
+            var categoria = $(".container-menu a.active").attr('id').split('menu-')[1];
+
+            let filtro = MENU[categoria];
+
+            let item = $.grep(filtro, (e, i) => { return e.id === id });
+
+            if(item.length > 0) {
+
+                let existe = $.grep(MEU_CARRINHO, (elemento, index) => { return elemento.id === id });
+
+                if(existe.length > 0) {
+                    let indice = MEU_CARRINHO.findIndex((obj) => obj.id == id);
+                    MEU_CARRINHO[indice].qntd = MEU_CARRINHO[indice].qntd + qntdAtual;
+                } else {
+                    item[0].qntd = qntdAtual;
+                    MEU_CARRINHO.push(item[0]);
+                }
+
+                alert('Item adicionado ao carrinho.');
+                $("#qntd-" + id).text(0);
+
+            }
+        }
     }
 }
 
 cardapio.templates = {
     item: `
         <div class="col-3 mb-5">
-            <div class="card card-item">
+            <div class="card card-item" id="\${id}">
                 <div class="img-produto">
                     <img src="\${img}" />
                 </div>
@@ -64,10 +104,10 @@ cardapio.templates = {
                     <b >R$ \${preco}</b>                                        
                 </p>
                 <div class="add-carrinho">
-                    <span class="btn-menos"><i class="fas fa-minus"></i></span>
-                    <span class="add-numero-itens">0</span>
-                    <span class="btn-mais"><i class="fas fa-plus"></i></span>
-                    <span class="btn btn-add"><i class="fa fa-shopping-bag"></i></span>
+                    <span class="btn-menos" onclick="cardapio.metodos.dimunuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
+                    <span class="add-numero-itens" id="qntd-\${id}">0</span>
+                    <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
+                    <span class="btn btn-add" onclick="cardapio.metodos.adicionarAoCarrinho('\${id}')"><i class="fa fa-shopping-bag"></i></span>
                 </div>
             </div>
         </div> 
