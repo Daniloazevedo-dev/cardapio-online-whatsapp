9455b1ebef1b67d45fa8abb36324cc47cd0949e9
Danilo Azevedo <danilo.ann@gmail.com>
2026-01-24 00:52:34 -0300

    feat: Implementa responsividade no cardapio, nas reservas e no footer

--- js/app.js
+++ js/app.js
@@ -430,7 +430,7 @@ cardapio.metodos = {
 
 cardapio.templates = {
     item: `
-        <div class="col-3 mb-5 wow fadeInUp">
+        <div class="col-12 col-lg-3 col-md-3 col-sm-6 mb-5 wow fadeInUp">
             <div class="card card-item" id="\${id}">
                 <div class="img-produto">
                     <img src="\${img}" />
